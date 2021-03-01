c213e56fa11775c5b4dcfe9a4cf2c4ff5df5fe22
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-01 12:12:35 +0000

    Initial Commit

---
+++ Floyds Triangle/Floyds Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Floyds_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n, p, q;
+
+            Console.Write("\n\n");
+            Console.Write("Print the Floyd's Triangle:\n");
+            Console.Write("-----------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input number of rows : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                { p = 1; q = 0; }
+                else
+                { p = 0; q = 1; }
+                for (j = 1; j <= i; j++)
+                    if (j % 2 == 0)
+                        Console.Write("{0}", p);
+                    else
+                        Console.Write("{0}", q);
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+} 
